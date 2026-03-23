6acb2a90f7d844daef2f6035c00cdd5c0fb0ecb2
GuilhermeCristaldoDev <guilherme.cristaldo.dev@gmail.com>
2026-03-23 18:10:02 -0300

    feat: add meat count to Session to tracking MeatNumber

--- meat-console-API/meat-console-API/Entities/Session.cs
+++ meat-console-API/meat-console-API/Entities/Session.cs
@@ -5,12 +5,20 @@
         public int Id { get; private set; }
         public DateTime CreatedAt { get; private set; }
         public DateTime? ClosedAt { get; private set; }
+        public int MeatCount { get; private set; }
         public bool IsActive { get; private set; }
 
         public Session()
         {
             CreatedAt = DateTime.Now;
+            MeatCount = 0;
             IsActive = true;
+        }
+
+        public int GetNextMeatNumber()
+        {
+            MeatCount++;
+            return MeatCount;
         }
 
         public void CloseSession()
